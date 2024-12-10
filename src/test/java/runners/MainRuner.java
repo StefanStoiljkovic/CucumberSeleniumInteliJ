6c0bffbbe43173d2b0d2f42ffacd460747738670
Stefan Stoiljkovic <stefanstoiljkovic8@gmail.com>
2024-12-10 19:34:42 +0100

    changes to the pom.xml

--- src/test/java/runners/MainRuner.java
+++ src/test/java/runners/MainRuner.java
@@ -2,6 +2,7 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
 
 @CucumberOptions(features = "src/test/resources/features",
                 glue ={"stepDefinitions"},
@@ -11,5 +12,7 @@ import io.cucumber.testng.CucumberOptions;
                 plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"}
         //komentar
 )
+
+@Test
 public class MainRuner extends AbstractTestNGCucumberTests {
 }
